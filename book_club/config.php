8ee01f4330b6ae990129cc68d1a737e3d19bb324
limweld <libradilla.limwel@gmail.com>
2016-11-20 15:37:06 +0800

    first commit

---
+++ book_club/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define( "DB_DSN", "mysql:host=localhost;dbname=mydatabase" );
+define( "DB_USERNAME", "root" );
+define( "DB_PASSWORD", "" );
+define( "PAGE_SIZE", 5 );
+define( "TBL_MEMBERS", "members" );
+define( "TBL_ACCESS_LOG", "accessLog" );
+?>
